c68b5428c8e0892e7574af01b2beeed425b24623
Bruno Silva <bruno.silva@jet.com.br>
2021-03-26 21:18:36 -0300

    create elements of page

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Bruno Silva - Landing page
 
 Create landing page using only javscript. All elements were created using javascript
+
+### 1° etapa
+
+Create page/elements in HTML using only commands javascript
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,15 @@
 const body = document.getElementById("body");
 
 //data
-const itensMenu = ['about', 'contact'];
-
+const itensMenu = ['download', 'contact'];
+const iconsRules = [
+    {icon: 'src/img/landing-page/ghost.gif', text: 'Truques de stories'},
+    {icon: 'src/img/landing-page/lightning.gif', text: 'Novos filtros e fontes'},
+    {icon: 'src/img/landing-page/angry-cat.gif', text: 'Receba gifs inéditos'},
+    {icon: 'src/img/landing-page/in-love.gif', text: 'Técnicas para stories mais interessantes'}
+];
+const itensInsta = ['src/img/carousel/insta-1.png', 'src/img/carousel/insta-2.png', 'src/img/carousel/insta-3.png', 'src/img/carousel/insta-4.png', 
+                    'src/img/carousel/insta-5.png', 'src/img/carousel/insta-6.png', 'src/img/carousel/insta-7.png'];
 
 //function create element
 function newElement(name, className, tipo){
@@ -19,7 +26,7 @@ header = newElement("header","header","header");
 //create element Logo
 logo = newElement("logo","logo","div");
 imgLogo = newElement("imgLogo","","img");
-imgLogo.src = "src/img/Logo.png";
+imgLogo.src = "src/img/9storm.png";
 
 //create element Menu e List Menu
 menu = newElement("menu","menu","div")
@@ -32,6 +39,11 @@ for(var i = 0; i < itensMenu.length; i++){
     li = newElement("li","item","li");
     span = newElement("span","","span");
     span.innerHTML = itensMenu[i];
+
+    if(itensMenu[i] == "download"){
+        link.className = "linkItem btnDownload";
+        span.innerHTML = "DOWNLOAD GRÁTIS";
+    }
 
     li.appendChild(span);
     link.appendChild(li);
@@ -47,15 +59,93 @@ header.appendChild(menu);
 
 
 //---------------------------------------------START FULLBANNER
-//create element fullbanner
+//create element section fullbanner
 divFullbanner = newElement("divFullbanner","fullbanner","section");
 
-//create element Logo
+//create element banner
 banner = newElement("banner","banner","img");
-banner.src = "src/img/fullbanner.png";
+banner.src = "src/img/fullbanner.jpg";
 divFullbanner.appendChild(banner);
+
+boxMessage = newElement("boxMessage","text-fullbanner","div");
+smallTxtDownload = newElement("smallTxtDownload","txt-download","small")
+smallTxtDownload.innerHTML = "Download Grátis";
+
+txtPrincipal = newElement("txtPrincipal","txt-principal","p")
+txtPrincipal.innerHTML = "Conteúdos criativos para stories do instagram";
+
+txtSecond = newElement("txtSecond","txt-second","span")
+txtSecond.innerHTML = "Aumente seguidores, faça melhores postagens, construa uma audiência fiel, faça conteúdo interessante e alavanque seus stories.";
+
+btnDownloadBanner = newElement("btnDownloadBanner","btnBanner","a")
+btnDownloadBanner.innerHTML = "Faça download";
+
+boxMessage.appendChild(smallTxtDownload);
+boxMessage.appendChild(txtPrincipal);
+boxMessage.appendChild(txtSecond);
+boxMessage.appendChild(btnDownloadBanner);
+divFullbanner.appendChild(boxMessage);
 //---------------------------------------------END FULLBANNER
+
+
+//---------------------------------------------START RULE
+//create element section rules with icon
+divRules = newElement("divRules","rules","section");
+rulesContainer = newElement("rulesContainer","container","div");
+
+titleRule = newElement("titleRule","txt-principal","p")
+titleRule.innerHTML = "Tudo que você precisa saber para criar stories criativas nas redes sociais";
+
+icons = newElement("icons","icons","div");
+
+listRules = newElement("listRules","listRules","ul")
+
+for(var i = 0; i < iconsRules.length; i++){
+    itemRules = newElement("itemRules","itemRules","li")
+    imgRule = newElement("imgRule","imgRule","img")
+    imgRule.setAttribute("src", iconsRules[i].icon);
+    spanRule = newElement("spanRule","spanRule","span")
+    spanRule.innerHTML = iconsRules[i].text;
+
+    itemRules.appendChild(imgRule);
+    itemRules.appendChild(spanRule);
+    listRules.appendChild(itemRules);
+}
+
+icons.appendChild(listRules);
+rulesContainer.appendChild(titleRule);
+rulesContainer.appendChild(icons);
+divRules.appendChild(rulesContainer);
+//---------------------------------------------END RULE
+
+
+//---------------------------------------------START CAROUSEL
+divCarousel = newElement("divCarousel","imagens-insta","section");
+divBoxCarousel = newElement("divBoxCarousel","","div");
+divBoxCarousel.setAttribute("id", "carousel-container");
+divCarouselItem = newElement("divCarouselItem","carousel-wrapper","div");
+divItem = newElement("divItem","carousel","div");
+
+divCarouselItem.appendChild(divItem);
+divBoxCarousel.appendChild(divCarouselItem);
+divCarousel.appendChild(divBoxCarousel);
+
+
+for(var i = 0; i < itensInsta.length; i++){
+    item = newElement("item","","div");
+    imgCarousel = newElement("imgCarousel","imgCarousel","img")
+    imgCarousel.setAttribute("src", itensInsta[i]);
+
+    item.appendChild(imgCarousel);
+    divItem.appendChild(item);
+}
+
+
+//divCarousel.appendChild(listCarousel);
+//---------------------------------------------END CAROUSEL
 
 
 body.appendChild(header);
 body.appendChild(divFullbanner);
+body.appendChild(divRules);
+body.appendChild(divCarousel);
